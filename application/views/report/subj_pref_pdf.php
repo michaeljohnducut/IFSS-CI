2a6dc93c845676c302f6a20404dae69791695812
annthonite <annthoniteb@gmail.com>
2018-10-03 21:25:20 +0800

    subj pref walang excel yahuuuu

--- application/views/report/subj_pref_pdf.php
+++ application/views/report/subj_pref_pdf.php
@@ -28,7 +28,66 @@
 
             <br>
 
-            <div style="margin-left: 5%;">
+            <div style="margin-left: 2%; margin-right: 2%">
+                <?php 
+                    $result2 = array();
+                    $result3 = array();
+                    foreach($faculty as $f)
+                    {
+                        $fac_id = $f[7];
+
+                        $result2 = $this->getdata_model->get_preftime($acadyr, $sem, $fac_id);
+                        $result3 = $this->getdata_model->get_prefsub($acadyr, $sem, $fac_id);
+
+                            echo '<div style="font-size:11px; page-break-inside: avoid;">
+                                    <table style="width:100%;">
+                                        <thead>
+                                           <tr>
+                                                <th>Faculty ID: '.$f[0].'</th>
+                                                <th>Faculty Name: '.$f[1].', '.$f[2].' '.$f[3].'</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>';
+
+                                        echo '<tr>
+                                                    <th>Preferred Day</th>
+                                                    <th>Preferred Time</th>
+                                                 </tr>';
+
+                                        foreach($result2 as $pt)
+                                        {
+                                            echo '<tr>
+                                                    <td>'.$pt[0].'</td>
+                                                    <td>'.$pt[1].'</td>
+                                                  </tr>';
+                                        }
+
+                                        echo '<tr>
+                                                    <th></th>
+                                                    <th></th>
+                                              </tr>
+                                              <tr>
+                                                    <th>Preferred Subjects</th>
+                                                    <th></th>
+                                              </tr>';
+                                        
+                                        foreach($result3 as $s)
+                                        {
+                                            echo '<tr>
+                                                    <td>'.$s[0].'</td>
+                                                    <td> </td>
+                                                  </tr> ';
+                                        }
+
+                            echo        '</tbody>
+                                    </table>
+                                </div><br><br>';
+                        
+                    }
+                ?>
+                </div>
+
+            <!-- <div style="margin-left: 5%;">
                 <div style="font-size:11px; page-break-inside: avoid;">
                 	<table>
                 		<thead>
@@ -72,6 +131,6 @@
                 		</tbody>
                 	</table>
                 </div>
-           	</div>
+           	</div> -->
 </body>
 </html>
